ddab0cf0e2975e861be5ec7ce2f2d3610a282b56
bridgeBook <johndoe@example.cok9mm10feet@yahoo.co.jp>
2024-10-28 22:23:28 +0900

    追加ボタン

--- strmemo/src/App.tsx
+++ strmemo/src/App.tsx
@@ -3,23 +3,30 @@ import StrUnit from "./component/StrUnit";
 import Memo from "./component/Memo";
 import { useState } from "react";
 
-function App() {
-  const [testaaa, setTestaaa] = useState({
-    1: 1,
-  });
+type TestCount = {
+  id: number;
+};
 
-  const test = () => {
-    // const newadd = [...testaaa];
+function App() {
+  const [strCount, setstrCount] = useState<TestCount[]>([]);
+
+  const AddStrmemo = () => {
+    console.log(strCount);
+    setstrCount([
+      ...strCount,
+      {
+        id: strCount.length,
+      },
+    ]);
   };
 
   return (
     <>
-      {/* <textarea className="flex addFusen flex-wrap m-10"></textarea> */}
-      <button className="addButton m-10" onClick={test}>
+      <button className="addButton m-10" onClick={AddStrmemo}>
         追加
       </button>
       <div>
-        {Object.keys(testaaa).map(() => (
+        {Object.keys(strCount).map(() => (
           <div className="ml-10">
             <div className="flex-col mt-10">
               <StrUnit />
